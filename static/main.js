57f5a7fcfd8f95c26978f58ab84def6cfb21c456
Richard Tomy <richiet.tomy@gmail.com>
2021-01-05 23:09:56 +0530

    Added report issue button

--- static/main.js
+++ static/main.js
@@ -13,7 +13,7 @@
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 17;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto, .report-error a', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
